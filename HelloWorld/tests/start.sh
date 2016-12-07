173a2202e717e735648c35192276bcf18285f8ac
Rob McCauley <mccaul@amazon.com>
2016-12-07 01:48:45 -0500

    readme

---
+++ HelloWorld/tests/start.sh
@@ -0,0 +1,13 @@
+# Test your local Node.JS lambda function with lambda-local
+# Install from https://www.npmjs.com/package/lambda-local
+#
+
+
+ lambda-local -l ../src/index.js -h handler -e events/launch.js
+
+# lambda-local -l ../src/index.js -h handler -e events/intent.js
+# lambda-local -l ../src/index.js -h handler -e events/intent-slot.js
+# lambda-local -l ../src/index.js -h handler -e events/help.js
+# lambda-local -l ../src/index.js -h handler -e events/stop.js
+# lambda-local -l ../src/index.js -h handler -e events/cancel.js
+
